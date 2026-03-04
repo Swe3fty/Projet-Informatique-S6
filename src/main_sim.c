16b0c85aa8a036cc33ca4a6f37ab28b6ac12232e
Gazpacho <145045112+Gaspard-Isen@users.noreply.github.com>
2026-03-04 08:55:06 +0100

    main_sim.c Update

--- src/main_sim.c
+++ src/main_sim.c
@@ -1,16 +1,47 @@
+#include <unistd.h>
 #include "consigne.h"
 #include "visualisationT.h"
 #include "regulation.h"
 #include "visualisationC.h"
+#include "simulateur.h"
 
 
 
 int main(void){
-    temp_t temperatures = {5,25};
-    float csgn = consigne(10.0);
+    
+    temp_t temperatures;
+    temperatures.interieure = 15.0;
+    temperatures.exterieure = 14.0;
     float cmd = 0.0;
-    float tabT[] = {15.0, 14.0, 13.0, 12.0, 11.0};
-    float csgn = 19.0;
+    float csgn = 0.0;
+    float prev_error = -999.0;
+    float I = 0.0;
+    struct simParam_s* param_ps = simConstruct(temperatures);
+
+    while(1){
+        //Lecture de la consigne
+        csgn = consigne(temperatures.interieure);
+
+        //Visualisatoin temp
+        visualisationT(temperatures);
+
+        //Calcul de la régulation
+        cmd = regulation(2,csgn,temperatures.interieure,&I,&prev_error);
+
+        //Afficher témoin
+        visualisationC(cmd);
+
+        //Envoie de la commande au simulateur
+        temperatures = simCalc(cmd, param_ps);
+
+        printf("Temperature interieure : %.1f°C, Temperature exterieure : %.1f°C\n", temperatures.interieure, temperatures.exterieure);
+        usleep(1000000);
+
+    }
+    simDestruct(param_ps);
+    
+
+    /*float tabT[] = {15.0, 14.0, 13.0, 12.0, 11.0};
     int taille = sizeof(tabT)/sizeof(float);
 
     visualisationT(temperatures);
@@ -19,7 +50,7 @@ int main(void){
     cmd = regulationTest(2,csgn,tabT,taille);
     printf("La commande du chauffage est : %f\n",cmd);
 
-    visualisationC(csgn);
+    visualisationC(csgn);*/
 
     return 0;
 }
